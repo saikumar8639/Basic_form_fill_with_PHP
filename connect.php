952544a694e8c5521e51419c773735973679994e
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-05 23:12:13 +0530

    first commit

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+    $firstName=$_POST['firstName'];
+    $lastName=$_POST['lastName'];
+    $male=$_POST['email'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $number=$_POST['number'];
+
+
+    
+?>
