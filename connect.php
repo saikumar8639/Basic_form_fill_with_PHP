d668894fbc40fb52cf872ae8d8adfb390c310ae1
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-05 23:37:00 +0530

    first commit

--- connect.php
+++ connect.php
@@ -6,6 +6,17 @@
     $password=$_POST['password'];
     $number=$_POST['number'];
 
+    $conn=new mysqli('localhost','root','','123456');
+    if ($conn->connect_error){
+        die('connection failed : '$conn->connect_error);
+    } else{
+        $stmt=$conn->prepare("insert into registration (firstName,lastName,gender,email,password,number) values(?,?,?,?,?,?)");
+        $stmt->bind_param("sssssi",$firstName,$lastName,$gender,$email,$password,$number);
+        $stmt->execute();
+        echo "registration successfully done.....";
+        $stmt->close();
+        $conn->close();
+    }
 
     
 ?>
