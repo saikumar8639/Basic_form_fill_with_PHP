76d14bc2b6f879aefb45808e48cf193c84cc699f
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-11-05 23:48:44 +0530

    first commit

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@
     $password=$_POST['password'];
     $number=$_POST['number'];
 
-    $conn=new mysqli('localhost','root','123456','test');
+    $conn = new mysqli('localhost','root','123456','test');
     if ($conn->connect_error){
         die("connection failed : ",$conn->connect_error);
     } else{
